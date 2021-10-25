5aed210fec913ed2d9c8076f2a94ba0fe2cbbb19
varduhimehrabyan <varduhimehrabyan@mail.ru>
2021-10-25 18:15:37 +0400

    nginx

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+var fs = require("fs");
 
-app.use(express.static('./client/dmn'))
-app.use(express.static('./client/user'))
+var https = require("https");
+var privateKey = fs.readFileSync("./server.key", "utf8");
+var certificate = fs.readFileSync("./server.crt", "utf8");
 
-app.use('/api', require('./Controllers'))
+var credentials = { key: privateKey, cert: certificate };
 
+const bak = require("./backupjob");
 
-app.use('/dmn', require('./dmn'))
-app.use('/user', require('./user'))
+bak();
 
-app.listen(4000);
+const express = require("express");
+const app = express();
+
+const http = express();
+
+http.get("*", function (req, res) {
+  res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.use(express.json());
+
+app.use(express.static("./client/user"));
+app.use(express.static("./client/dmn"));
+
+app.use("/api", require("./Controllers"));
+
+app.use("/dmn/", require("./dmn"));
+app.use("/", require("./user"));
+
+var httpsServer = https.createServer(credentials, app);
+
+http.listen(80);
+httpsServer.listen(443);
